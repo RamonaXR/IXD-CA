702b8ccda52f0d66e69c534afb7dab0c5d9159e1
RamonaXR <Ramonaelise@gmail.com>
2023-12-02 13:28:43 +0100

    Update index.js

--- js/products/index.js
+++ js/products/index.js
@@ -28,8 +28,8 @@ function removeSlashAndHtml(inputString) {
     let modifiedString = inputString.replace(/\//g, '').replace(/\.html/g, '');
     return modifiedString;
 }
-switch (location.pathname) {
-    case "/product":  //Will not load on liveserver, but I am aware of why and what to do different in the future.
+switch (removeSlashAndHtml(location.pathname))  {
+    case "product":  //Will not load on liveserver, but I am aware of why and what to do different in the future.
         try {
             createHtmlHome (products, lightweight);
         } catch (error) {
@@ -37,7 +37,7 @@ switch (location.pathname) {
             console.log("Ups, an error occured");
         }
         break; 
-    case "/spesific": //Same issue as on product. I am aware of why and in the future I will make a better filestructure.
+    case "spesific": //Same issue as on product. I am aware of why and in the future I will make a better filestructure.
         fetchProduct();
         break;
     default:
